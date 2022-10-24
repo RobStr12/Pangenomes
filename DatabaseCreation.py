9a0fbedd83f0494ff0dd12f5fe91c5e0208ac10b
Robin <stragierrobin@gmail.com>
2022-10-24 14:32:25 +0200

    main file

--- DatabaseCreation.py
+++ DatabaseCreation.py
@@ -1,9 +1,15 @@
 from typedb.client import TypeDB, SessionType, TransactionType
 
+path = "./PhoneCallScheme.tql"
+
 with TypeDB.core_client("localhost:1729") as client:
-    exists = client.databases().contains("Social Network")
-    if exists is False:
-        client.databases().create("Social Network")
-        with client.session("Social Network", SessionType.DATA) as session:
-            with session.transaction(TransactionType.WRITE) as transaction:
-                print("OK")
+    exists = client.databases().contains("Social_Network")
+    if exists:
+        client.databases().get("Social_Network").delete()
+    client.databases().create("Social_Network")
+    with client.session("Social_Network", SessionType.SCHEMA) as session:
+        with session.transaction(TransactionType.WRITE) as transaction:
+            with open(path, "r") as data:
+                query = data.read()
+                transaction.query().define(query)
+                transaction.commit()
---
+++ TqlFileReader.py
@@ -0,0 +1,4 @@
+path = "./PhoneCallScheme.tql"
+
+with open(path, "r")as data:
+    print(data.read())
